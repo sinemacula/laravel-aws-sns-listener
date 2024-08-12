95afe476af1a9bbc8399ac3e9c427b174e9d0191
Ben Carey <bdmc@sinemacula.co.uk>
2024-08-12 21:15:57 +0100

    Added test notification

---
+++ src/Entities/Messages/TestNotification.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SineMacula\Aws\Sns\Entities\Messages;
+
+use SineMacula\Aws\Sns\Entities\Messages\Contracts\NotificationInterface;
+
+/**
+ * AWS SNS test notification instance.
+ *
+ * @author      Ben Carey <bdmc@sinemacula.co.uk>
+ * @copyright   2024 Sine Macula Limited.
+ */
+class TestNotification extends Notification implements NotificationInterface { }
